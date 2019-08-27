1ab75cf7be673d458f3d12d7053c853368f7c6b0
Marcin <sarnik21@gmail.com>
2019-08-27 20:43:41 +0200

    Skonfigurowano pom.xml

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,8 +3,13 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
+
+        String passW = null;
+
+        PassBreaker passBreaker = new PassBreaker();
+        passBreaker.passBreaker("Marcin");
+
+
         System.out.println("Hello World");
-
-
     }
 }
